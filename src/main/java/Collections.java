65c9953870eb5ebd58fa60ca1bde1302f2bed84e
lbrav <l.braverman.l@live.co.uk>
2021-12-07 19:24:07 +0000

    "For karem"

---
+++ src/main/java/Collections.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class Collections {
+
+
+    public static void main(String[] args) {
+
+
+        String[] anExampleArray = new String[5]; // <- Array
+
+        anExampleArray[0] = "Karem";
+        anExampleArray[1] = "Luke";
+        anExampleArray[2] = "mum";
+
+        System.out.println(Arrays.asList(anExampleArray));
+
+
+
+
+//
+//        ArrayList<String> anArrayList = new ArrayList<>(); //ArrayList
+//
+//        anArrayList.add("Karem");
+//        anArrayList.add("Luke");
+//
+//        System.out.println(anArrayList.toString());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //Another time
+
+//        LinkedList linkedList = new LinkedList(); // !!!!
+//
+//        HashMap hashMap = new HashMap();
+//
+//        TreeSet treeSet = new TreeSet();
+
+
+
+
+    }
+}
